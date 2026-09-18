bdca062bfc25646e8150f52a42a5183df3877b4f
Gemma Mullan <20083580@setu.ie>
2026-09-18 16:51:20 +0100

    Delete a task

--- tasky/src/App.jsx
+++ tasky/src/App.jsx
@@ -16,6 +16,13 @@ function App() {
     tasks[taskIndex].done = !tasks[taskIndex].done;
     setTaskState({tasks});
   }
+
+    const deleteHandler = (taskIndex) => {
+    const tasks = [...taskState.tasks];
+    tasks.splice(taskIndex, 1);
+    setTaskState({tasks});
+  } 
+
   
     return (
     <div className="container">
@@ -29,6 +36,8 @@ function App() {
       key={task.id}
       done={task.done}
       markDone={() => doneHandler(index)}
+      deleteTask = {() => deleteHandler(index)}
+
     />
   ))} 
   
